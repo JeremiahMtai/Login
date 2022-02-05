10eb5f7f6ab0997d581ee5d854173abace422257
JeremiahMtai <jeremiahmtai99@gmail.com>
2022-02-05 19:44:54 +0300

    Modal Login Page Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Login Page with Background Image Example</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body style="background: goldenrod;">
+<hr>
+<br>
+<div class="container">
+  <div id="login-form">
+    <h3>User Login</h3>
+  </div>
+  <fieldset>
+            <form id="form-login" action="">
+                <input type="text" autofocus id="un" placeholder="username" required autocomplete="off">
+                <input type="password" id="up" placeholder="password" required autocomplete="off">
+                <input type="submit" name="Log" value="Login">
+            </form>
+        </fieldset>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
